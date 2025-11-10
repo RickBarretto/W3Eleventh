8274455de8f27bb58bdc1806fc1625b74ae4cfcd
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-09 22:11:50 -0300

    Move test to outer folder

--- contracts/test/test_auctions.py
+++ contracts/test/test_auctions.py
@@ -23,33 +23,7 @@ def cards():
 # =========== Scenario 1 ===========
 
 
-@given("a seller without any active auction", target_fixture="seller_without_active_auction")
-def seller_without_active_auction(trades):
-    seller = boa.env.generate_address()
-    auction = trades.auction_of(seller)
 
-    assert auction.seller == ZERO_ADDRESS
-    return seller
-
-
-@when("the seller auctions a card", target_fixture="auction_card")
-def auction_card(seller_without_active_auction, trades, cards):
-    owner = seller_without_active_auction
-    card = cards.card("Card A", 99)
-
-    with boa.env.prank(owner):
-        trades.auction_card(card, 7)
-
-    return owner
-
-
-@then("the Auction should be registered.")
-def auction_active_in_store(auction_card, trades):
-    owner = auction_card
-    auction = trades.auction_of(owner)
-
-    assert auction.card.name == "Card A"
-    assert auction.card.power == 99
 
 
 # =========== Scenario 2 ===========
