75f0a2e6790b16eb950301c080a7b30b27e83f30
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-14 15:10:39 -0300

    [Admin] type tests

--- contracts/tests/admin/test_register.py
+++ contracts/tests/admin/test_register.py
@@ -1,36 +1,43 @@
 import boa
+from boa.util.abi import Address
 import pytest
 from pytest_bdd import *
 
-from contracts.src import Admin
+from contracts.src import Admin as Admins
+
+
+type Owner = Address
+type Administrator = Address
+type Customer = Address
+type Candidate = Address
 
 
 @pytest.fixture
 def admins():
-    return Admin.deploy()
+    return Admins.deploy()
 
 
 @given("I'm an Owner", target_fixture="owner")
-def owner(admins):
-    owner = boa.env.eoa
+def owner(admins) -> Owner:
+    owner: Address = boa.env.eoa
     assert admins.is_admin(owner)
     return owner
 
 @given("I'm an Administrator", target_fixture="admin")
-def admin(admins):
-    address = boa.env.generate_address()
+def admin(admins) -> Administrator:
+    address: Address = boa.env.generate_address()
     admins.add(address)
     assert admins.is_admin(address)
     return address
 
 @given("I'm a Customer", target_fixture="customer")
-def customer(admins):
-    address = boa.env.generate_address()
+def customer(admins) -> Customer:
+    address: Address = boa.env.generate_address()
     assert not admins.is_admin(address)
     return address
 
 @given("I have a Candidate", target_fixture="candidate")
-def candidate():
+def candidate() -> Candidate:
     return boa.env.generate_address()
 
 
@@ -40,11 +47,11 @@ def test_successful_registering():
     pass
 
 @when("I register this new address as a new admin")
-def try_register(admins, owner, candidate):
+def try_register(admins, candidate: Candidate):
     admins.add(candidate)
 
 @then("the new admin should be registered successfully")
-def should_be_registered(admins, candidate):
+def should_be_registered(admins, candidate: Candidate):
     assert admins.is_admin(candidate)
 
 
@@ -54,11 +61,11 @@ def test_customer_trying_to_register_new_admin():
     pass
 
 @when("I try to register this new address as admin")
-def try_register(admins, customer, candidate):
+def try_register():
     pass
 
 @then('it should rollover "Must be EOA"')
-def should_rollover(admins, customer, candidate):
+def should_rollover(admins, customer: Customer, candidate: Candidate):
     with boa.env.prank(customer):
         with boa.reverts("Must be EOA"):
             admins.add(candidate)
@@ -75,7 +82,7 @@ def try_to_register():
     pass
 
 @then('it should rollover "Must be EOA" (Admin)')
-def should_rollover(admins, admin, candidate):
+def should_rollover(admins, admin: Administrator, candidate: Candidate):
     with boa.env.prank(admin):
         with boa.reverts("Must be EOA"):
             admins.add(candidate)
