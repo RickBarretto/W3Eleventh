cedaccae095ff183d6d9a7cc54de5e2cf4881167
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-14 15:26:49 -0300

    [Admin] Add failing test

--- contracts/tests/admin/test_list.py
+++ contracts/tests/admin/test_list.py
@@ -1,9 +1,13 @@
 import boa
+from boa.util.abi import Address
 import pytest
 from pytest_bdd import *
 
 from contracts.src import Admin
 
+
+type Administrator = Address
+type Customer = Address
 
 @pytest.fixture
 def admins():
@@ -14,9 +18,15 @@ def admins():
 def is_admin(admins):
     assert admins.is_admin(boa.env.eoa)
     boa.env.eoa
+    
+@given("I am not an Administrator", target_fixture="customer")
+def customer(admins) -> Customer:
+    address: Address = boa.env.generate_address()
+    assert not admins.is_admin(address)
+    return address
 
 @given("there are multiple administrators registered", target_fixture="expected_admins")
-def have_multiples(admins):
+def have_multiples(admins) -> list[Administrator]:
     new_admin = boa.env.generate_address()
     admins.add(new_admin)
     
@@ -32,9 +42,25 @@ def test_list_all_admins():
     pass
 
 @when("I try to list all administrators¹", target_fixture="actual_admins")
-def list_admins(admins):
+def list_admins(admins) -> list[Administrator]:
     return admins.all()
 
 @then("I should see the list of all registered administrators")
-def check_admin_list(expected_admins, actual_admins):
+def check_admin_list(expected_admins: list[Administrator], actual_admins: list[Administrator]):
     assert actual_admins == expected_admins
+
+
+
+@scenario("Admin.feature", "No Permission to Listing")
+def test_cannot_list():
+    pass
+
+@when("I try to list all administrators²")
+def try_list():
+    pass
+
+@then('it should rollover "Must be Admin"')
+def should_fail(admins, customer: Customer):
+    with boa.env.prank(customer):
+        with boa.reverts("Must be Admin"):
+            admins.all()
