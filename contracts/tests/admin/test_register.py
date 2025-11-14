352d3b95d76d7cfb20bb45a0486b841378f6f56e
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-14 00:03:27 -0300

    [Admin] Rename actors
    Non-Admin User -> Customer
    Admin User -> Administrator

--- contracts/tests/admin/test_register.py
+++ contracts/tests/admin/test_register.py
@@ -16,18 +16,18 @@ def owner(admins):
     assert admins.is_admin(owner)
     return owner
 
-@given("I am a not Externally Owned Admin", target_fixture="admin_user")
-def admin_user(admins):
-    admin_user = boa.env.generate_address()
-    admins.add(admin_user)
-    assert admins.is_admin(admin_user)
-    return admin_user
+@given("I'm an Administrator", target_fixture="admin")
+def admin(admins):
+    address = boa.env.generate_address()
+    admins.add(address)
+    assert admins.is_admin(address)
+    return address
 
-@given("I am not an Admin", target_fixture="non_admin")
-def non_admin(admins):
-    some_address = boa.env.generate_address()
-    assert not admins.is_admin(some_address)
-    return some_address
+@given("I'm a Customer", target_fixture="customer")
+def customer(admins):
+    address = boa.env.generate_address()
+    assert not admins.is_admin(address)
+    return address
 
 @given("I have some address", target_fixture="some_address")
 def some_address():
@@ -54,9 +54,10 @@ def test_register_admin_by_user():
     pass
 
 @when("I try to register this new address as admin")
-def register_attempt(admins, non_admin, some_address):
-    with boa.reverts("Must be EOA"):
-        admins.add(some_address, sender=non_admin)
+def register_attempt(admins, customer, some_address):
+    with boa.env.prank(customer):
+        with boa.reverts("Must be EOA"):
+            admins.add(some_address)
 
 @then('it should rollover "Must be EOA"')
 def should_rollover(admins, some_address):
@@ -69,8 +70,8 @@ def test_register_admin_by_user_admin():
     pass
 
 @when("I try to register this new address as admin (Admin)")
-def register_attempt_admin(admins, admin_user, some_address):
-    with boa.env.prank(admin_user):
+def register_attempt_admin(admins, admin, some_address):
+    with boa.env.prank(admin):
         with boa.reverts("Must be EOA"):
             admins.add(some_address)
 
--- features/Admin.feature
+++ features/Admin.feature
@@ -20,13 +20,13 @@ Feature: Admin Management
         Then the new admin should be registered successfully
 
     Scenario: No Permission to Register
-        Given I am not an Admin
+        Given I'm a Customer
             And I have some address
         When I try to register this new address as admin
         Then it should rollover "Must be EOA"
 
     Scenario: No Permission to Register (Admin)
-        Given I am a not Externally Owned Admin
+        Given I'm an Administrator
             And I have some address
         When I try to register this new address as admin (Admin)
         Then it should rollover "Must be EOA" (Admin)
