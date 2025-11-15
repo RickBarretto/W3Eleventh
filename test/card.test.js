f196bf544420e3546bc71609b437746b5da92fa0
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-15 00:25:03 -0300

    Format file

--- test/card.test.js
+++ test/card.test.js
@@ -1,51 +1,48 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
-describe("CardStore (card tests)", function () {
-    let Game, game, owner, player;
+describe("CardStore (card tests)", () => {
+    let Game, game, owner, player
 
-    beforeEach(async function () {
-        [owner, player] = await ethers.getSigners();
+    beforeEach(async () => {
+        [owner, player] = await ethers.getSigners()
 
-        Game = await ethers.getContractFactory("CardGame");
-        game = await Game.deploy();
+        Game = await ethers.getContractFactory("CardGame")
+        game = await Game.deploy()
 
-        await game.deployed();
+        await game.deployed()
+    })
 
-    });
-
-    it("rewards unique cards and stores attributes", async function () {
-        const address = player.address;
+    it("rewards unique cards and stores attributes", async () => {
+        const address = player.address
 
         const cardIdNumber = await cardIdFromTransaction(
             await game.awardUniqueCardTo(address, "Defender", 70)
-        );
-        const cardInfo = await game.cardDetails(cardIdNumber);
+        )
+        const cardInfo = await game.cardDetails(cardIdNumber)
 
-        expect(cardInfo[0]).to.equal("Defender");
-        expect(cardInfo[1].toNumber()).to.equal(70);
-        expect(cardInfo[2]).to.equal(address);
-
+        expect(cardInfo[0]).to.equal("Defender")
+        expect(cardInfo[1].toNumber()).to.equal(70)
+        expect(cardInfo[2]).to.equal(address)
 
         const cardIdNumber2 = await cardIdFromTransaction(
             await game.awardUniqueCardTo(address, "Defender", 70)
-        );
+        )
 
-        expect(cardIdNumber).to.not.equal(cardIdNumber2);
-        expect(await cardCount(game, address)).to.equal(2);
+        expect(cardIdNumber).to.not.equal(cardIdNumber2)
+        expect(await cardCount(game, address)).to.equal(2)
+    })
+})
 
-    });
-});
-
-async function cardIdFromTransaction(tx1) {
+const cardIdFromTransaction = async (tx1) => {
     return (await tx1.wait())
         .events
         .find((e) => e.event === 'CardWasAwarded')
         .args
         .cardId
-        .toNumber();
+        .toNumber()
 }
 
-async function cardCount(cardGame, a) {
-    return (await cardGame.numberOfCardsInTeam(a)).toNumber();
+const cardCount = async (cardGame, a) => {
+    return (await cardGame.numberOfCardsInTeam(a)).toNumber()
 }
