cc640451e783d1b94940329fee841ed16dcd2eb1
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-15 00:25:44 -0300

    Rename variables

--- test/team.test.js
+++ test/team.test.js
@@ -2,26 +2,26 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("TeamManager (team tests)", function () {
-    let CardGame, cardGame, player;
+    let Game, game, player;
 
     beforeEach(async function () {
         [, player] = await ethers.getSigners();
 
-        CardGame = await ethers.getContractFactory("CardGame");
-        cardGame = await CardGame.deploy();
+        Game = await ethers.getContractFactory("CardGame");
+        game = await Game.deploy();
 
-        await cardGame.deployed();
+        await game.deployed();
     });
 
     it("creates a team and grants 5 starter cards", async function () {
         const playerAddress = player.address;
 
-        await cardGame.createNewTeamFor(playerAddress);
-        expect(await cardGame.doesTeamExist(playerAddress)).to.equal(true);
-        expect(await countCards(cardGame, playerAddress)).to.equal(5);
+        await game.createNewTeamFor(playerAddress);
+        expect(await game.doesTeamExist(playerAddress)).to.equal(true);
+        expect(await countCards(game, playerAddress)).to.equal(5);
 
-        await cardGame.createNewTeamFor(playerAddress);
-        expect(await countCards(cardGame, playerAddress)).to.equal(5);
+        await game.createNewTeamFor(playerAddress);
+        expect(await countCards(game, playerAddress)).to.equal(5);
     });
 });
 
