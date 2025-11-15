958520c3ff84068971315b16cec6b15e7a9128a1
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-15 00:23:34 -0300

    Rename variables

--- test/card.test.js
+++ test/card.test.js
@@ -2,37 +2,37 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("CardStore (card tests)", function () {
-    let CardGame, cardGame, owner, player;
+    let Game, game, owner, player;
 
     beforeEach(async function () {
         [owner, player] = await ethers.getSigners();
 
-        CardGame = await ethers.getContractFactory("CardGame");
-        cardGame = await CardGame.deploy();
+        Game = await ethers.getContractFactory("CardGame");
+        game = await Game.deploy();
 
-        await cardGame.deployed();
+        await game.deployed();
 
     });
 
     it("rewards unique cards and stores attributes", async function () {
-        const host = player.address;
+        const address = player.address;
 
         const cardIdNumber = await cardIdFromTransaction(
-            await cardGame.awardUniqueCardTo(host, "Defender", 70)
+            await game.awardUniqueCardTo(address, "Defender", 70)
         );
-        const cardInfo = await cardGame.cardDetails(cardIdNumber);
+        const cardInfo = await game.cardDetails(cardIdNumber);
 
         expect(cardInfo[0]).to.equal("Defender");
         expect(cardInfo[1].toNumber()).to.equal(70);
-        expect(cardInfo[2]).to.equal(host);
+        expect(cardInfo[2]).to.equal(address);
 
 
         const cardIdNumber2 = await cardIdFromTransaction(
-            await cardGame.awardUniqueCardTo(host, "Defender", 70)
+            await game.awardUniqueCardTo(address, "Defender", 70)
         );
 
         expect(cardIdNumber).to.not.equal(cardIdNumber2);
-        expect(await cardCount(cardGame, host)).to.equal(2);
+        expect(await cardCount(game, address)).to.equal(2);
 
     });
 });
