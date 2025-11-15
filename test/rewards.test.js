1fcd2bb378d7c22a79cbd9b853275568823ff0ca
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-14 23:48:04 -0300

    Make test more readable

--- test/rewards.test.js
+++ test/rewards.test.js
@@ -6,32 +6,59 @@ describe("Rewards (claim tests)", function () {
 
     beforeEach(async function () {
         [, player1, player2] = await ethers.getSigners();
+
         CardGame = await ethers.getContractFactory("CardGame");
         cardGame = await CardGame.deploy();
+
         await cardGame.deployed();
     });
 
     it("allows winner to claim reward and mints a card", async function () {
-        const a = player1.address;
-        const b = player2.address;
-        const tx1 = await cardGame.awardUniqueCardTo(a, "Striker", 90);
-        const id1 = (await tx1.wait()).events.find((e) => e.event === 'CardWasAwarded').args.cardId.toNumber();
-        const tx2 = await cardGame.awardUniqueCardTo(b, "Defender", 70);
-        const id2 = (await tx2.wait()).events.find((e) => e.event === 'CardWasAwarded').args.cardId.toNumber();
+        const host = player1.address;
+        const guest = player2.address;
 
-        await cardGame.conductMatchBetween(id1, id2);
-        expect((await cardGame.pendingRewardCountFor(a)).toNumber()).to.equal(1);
+        const hostsCard = await cardIdFromTransaction(
+            await cardGame.awardUniqueCardTo(host, "Striker", 90)
+        );
+        
+        const guestsCard = await cardIdFromTransaction(
+            await cardGame.awardUniqueCardTo(guest, "Defender", 70)
+        );
+        
+        await cardGame.conductMatchBetween(hostsCard, guestsCard);
+        expect(await rewardCount(cardGame, host)).to.equal(1);
 
-        const claimTx = await cardGame.connect(player1).claimPendingReward();
-        const rc = await claimTx.wait();
-        const ev = rc.events.find((e) => e.event === 'RewardWasClaimed');
-        const newId = ev.args.cardId.toNumber();
-        // team card count increased
-        expect((await cardGame.numberOfCardsInTeam(a)).toNumber()).to.be.greaterThan(1);
-        // pending rewards decremented
-        expect((await cardGame.pendingRewardCountFor(a)).toNumber()).to.equal(0);
+        const newId = await claimReward(cardGame, player1);
+        expect(await cardCount(cardGame, host)).to.be.greaterThan(1);
+        expect(await rewardCount(cardGame, host)).to.equal(0);
 
-        const card = await cardGame.cardDetails(newId);
-        expect(card[2]).to.equal(a);
+        const cardInfo = await cardGame.cardDetails(newId);
+        expect(cardInfo[2]).to.equal(host);
     });
 });
+
+async function cardIdFromTransaction(tx1) {
+    return (await tx1.wait())
+        .events
+        .find((e) => e.event === 'CardWasAwarded')
+        .args
+        .cardId
+        .toNumber();
+}
+
+async function claimReward(cardGame, player1) {
+    const claimTx = await cardGame.connect(player1).claimPendingReward();
+    const rc = await claimTx.wait();
+    const ev = rc.events.find((e) => e.event === 'RewardWasClaimed');
+    const newId = ev.args.cardId.toNumber();
+    return newId;
+}
+
+async function rewardCount(cardGame, a) {
+    return (await cardGame.pendingRewardCountFor(a)).toNumber();
+}
+
+async function cardCount(cardGame, a) {
+    return (await cardGame.numberOfCardsInTeam(a)).toNumber();
+}
+
