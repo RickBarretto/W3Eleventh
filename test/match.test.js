460892d604df7d462d75eb1c6a01885f9e375022
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-15 00:18:24 -0300

    Rename variables

--- test/match.test.js
+++ test/match.test.js
@@ -2,47 +2,41 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("MatchEngine (match tests)", function () {
-    let CardGame, cardGame, player1, player2;
+    let Game, game, host, guest;
 
     beforeEach(async function () {
-        [, player1, player2] = await ethers.getSigners();
-        CardGame = await ethers.getContractFactory("CardGame");
-        cardGame = await CardGame.deploy();
-        await cardGame.deployed();
+        [, host, guest] = await ethers.getSigners();
+        Game = await ethers.getContractFactory("CardGame");
+        game = await Game.deploy();
+        await game.deployed();
     });
 
     it("awards pending reward to winning card owner", async function () {
-        const host = player1.address;
-        const guest = player2.address;
-
         const hostsCard = await cardIdFromTransaction(
-            await cardGame.awardUniqueCardTo(host, "Striker", 90)
+            await game.awardUniqueCardTo(host.address, "Striker", 90)
         );
 
         const guestsCard = await cardIdFromTransaction(
-            await cardGame.awardUniqueCardTo(guest, "Defender", 70)
+            await game.awardUniqueCardTo(guest.address, "Defender", 70)
         );
 
-        await cardGame.conductMatchBetween(hostsCard, guestsCard);
-        expect(await rewardCount(cardGame, host)).to.equal(1);
-        expect(await rewardCount(cardGame, guest)).to.equal(0);
+        await game.conductMatchBetween(hostsCard, guestsCard);
+        expect(await rewardCount(game, host.address)).to.equal(1);
+        expect(await rewardCount(game, guest.address)).to.equal(0);
     });
 
     it("does not award pending reward on draw", async function () {
-        const host = player1.address;
-        const guest = player2.address;
-
         const hostsCard = await cardIdFromTransaction(
-            await cardGame.awardUniqueCardTo(host, "Mid", 80)
+            await game.awardUniqueCardTo(host.address, "Mid", 80)
         );
 
         const guestsCard = await cardIdFromTransaction(
-            await cardGame.awardUniqueCardTo(guest, "Mid", 80)
+            await game.awardUniqueCardTo(guest.address, "Mid", 80)
         );
 
-        await cardGame.conductMatchBetween(hostsCard, guestsCard);
-        expect(await rewardCount(cardGame, host)).to.equal(0);
-        expect(await rewardCount(cardGame, guest)).to.equal(0);
+        await game.conductMatchBetween(hostsCard, guestsCard);
+        expect(await rewardCount(game, host)).to.equal(0);
+        expect(await rewardCount(game, guest)).to.equal(0);
     });
 });
 
