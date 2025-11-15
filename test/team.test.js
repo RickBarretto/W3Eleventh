516135c8d6316f0b16ce5f2193e3ee2be27afb9e
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-15 00:26:37 -0300

    Format file

--- test/team.test.js
+++ test/team.test.js
@@ -1,31 +1,30 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
-describe("TeamManager (team tests)", function () {
-    let Game, game, player;
+describe("TeamManager (team tests)", () => {
+    let Game, game, player
 
-    beforeEach(async function () {
-        [, player] = await ethers.getSigners();
+    beforeEach(async () => {
+        [, player] = await ethers.getSigners()
 
-        Game = await ethers.getContractFactory("CardGame");
-        game = await Game.deploy();
+        Game = await ethers.getContractFactory("CardGame")
+        game = await Game.deploy()
 
-        await game.deployed();
-    });
+        await game.deployed()
+    })
 
-    it("creates a team and grants 5 starter cards", async function () {
-        const playerAddress = player.address;
+    it("creates a team and grants 5 starter cards", async () => {
+        const playerAddress = player.address
 
-        await game.createNewTeamFor(playerAddress);
-        expect(await game.doesTeamExist(playerAddress)).to.equal(true);
-        expect(await countCards(game, playerAddress)).to.equal(5);
+        await game.createNewTeamFor(playerAddress)
+        expect(await game.doesTeamExist(playerAddress)).to.equal(true)
+        expect(await countCards(game, playerAddress)).to.equal(5)
 
-        await game.createNewTeamFor(playerAddress);
-        expect(await countCards(game, playerAddress)).to.equal(5);
-    });
-});
+        await game.createNewTeamFor(playerAddress)
+        expect(await countCards(game, playerAddress)).to.equal(5)
+    })
+})
 
-async function countCards(cardGame, playerAddress) {
-    return (await cardGame.numberOfCardsInTeam(playerAddress)).toNumber();
+const countCards = async (cardGame, playerAddress) => {
+    return (await cardGame.numberOfCardsInTeam(playerAddress)).toNumber()
 }
-
