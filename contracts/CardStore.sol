59694ebb308297317cbe5ebbc8054da382195d0d
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-14 23:13:56 -0300

    Make internal state more readable

--- contracts/CardStore.sol
+++ contracts/CardStore.sol
@@ -8,18 +8,18 @@ contract CardStore {
         address owner;
     }
 
-    uint256 public nextCardId;
+    uint256 public cardCount;
     mapping(uint256 => Card) public cards;
 
     function createCard(address to, string calldata name, uint256 power) external returns (uint256) {
-        nextCardId += 1;
-        uint256 id = nextCardId;
+        cardCount += 1;
+        uint256 id = cardCount;
         cards[id] = Card({name: name, power: power, owner: to});
         return id;
     }
 
     function getCard(uint256 id) external view returns (string memory, uint256, address) {
-        Card memory c = cards[id];
-        return (c.name, c.power, c.owner);
+        Card memory card = cards[id];
+        return (card.name, card.power, card.owner);
     }
 }
