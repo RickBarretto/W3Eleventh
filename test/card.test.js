480f3075700e6b50c90478c9a67073a3d7857ea6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-14 23:57:22 -0300

    Make test more readable

--- test/card.test.js
+++ test/card.test.js
@@ -15,27 +15,37 @@ describe("CardStore (card tests)", function () {
     });
 
     it("rewards unique cards and stores attributes", async function () {
-        const playerAddress = player.address;
+        const host = player.address;
 
-        const claimTransaction = await cardGame.awardUniqueCardTo(playerAddress, "Defender", 70);
-        const transactionReceipt = await claimTransaction.wait();
-
-        const claimedCardEvent = transactionReceipt.events.find((e) => e.event === 'CardWasAwarded');
-        const cardIdNumber = claimedCardEvent.args.cardId.toNumber();
+        const cardIdNumber = await cardIdFromTransaction(
+            await cardGame.awardUniqueCardTo(host, "Defender", 70)
+        );
         const cardInfo = await cardGame.cardDetails(cardIdNumber);
 
         expect(cardInfo[0]).to.equal("Defender");
         expect(cardInfo[1].toNumber()).to.equal(70);
-        expect(cardInfo[2]).to.equal(playerAddress);
+        expect(cardInfo[2]).to.equal(host);
 
 
-        const claimTransaction2 = await cardGame.awardUniqueCardTo(playerAddress, "Defender", 70);
-        const transactionReceipt2 = await claimTransaction2.wait();
-        const claimedCardEvent2 = transactionReceipt2.events.find((e) => e.event === 'CardWasAwarded');
-        const cardIdNumber2 = claimedCardEvent2.args.cardId.toNumber();
+        const cardIdNumber2 = await cardIdFromTransaction(
+            await cardGame.awardUniqueCardTo(host, "Defender", 70)
+        );
 
         expect(cardIdNumber).to.not.equal(cardIdNumber2);
-        expect((await cardGame.numberOfCardsInTeam(playerAddress)).toNumber()).to.equal(2);
+        expect(await cardCount(cardGame, host)).to.equal(2);
 
     });
 });
+
+async function cardIdFromTransaction(tx1) {
+    return (await tx1.wait())
+        .events
+        .find((e) => e.event === 'CardWasAwarded')
+        .args
+        .cardId
+        .toNumber();
+}
+
+async function cardCount(cardGame, a) {
+    return (await cardGame.numberOfCardsInTeam(a)).toNumber();
+}
