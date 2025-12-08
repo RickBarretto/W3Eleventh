1935a8b66baafc1b85f2aa6797ad2348653d2ee6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-12-07 22:19:53 -0300

    Remove Quit option
    This is already present on the default command: Ctrl + P

--- app/ui/main.py
+++ app/ui/main.py
@@ -3,12 +3,8 @@ from textual.widgets import Footer, Header
 
 class Eleventh(App):
 
-    BINDINGS = [("escape", "quit", "Quit")]
-
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
         yield Footer()
 
-    def action_quit(self) -> None:
-        self.exit()
 
